c7e650d4d105c78745d7a849c044850782818944
CuongCter <cuongcter442002@gmail.com>
2023-08-30 15:44:42 +0700

    :heavy_plus_sign: coding load and clear image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,89 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../assets/home.css'
 import Header from '../components/Header'
 import Card from '../components/Card'
+import axios from 'axios'
 const Home = () => {
-    const [number,setNumber] = useState('')
-    const handleNumberChange = (number) => {
-        setNumber(number)
+  const [number, setNumber] = useState('')
+  const [data, setData] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const handleSetNumber = (e) => {
+    setNumber(e.target.value);
+    setSelectedCards([]);
+  };
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const res = await axios.get(
+          `https://dog.ceo/api/breeds/image/random/${number}`
+        );
+        setData(res.data.message);
+      } catch (error) {
+        console.error(error);
       }
-    
-    
+    };
+    fetchImage();
+  }, [number]);
+  const handleCardSelection = (index) => {
+    if (selectedCards.includes(index)) {
+      setSelectedCards(selectedCards.filter((cardIndex) => cardIndex !== index));
+    } else {
+      setSelectedCards([...selectedCards, index]);
+    }
+  };
+  
+  const handleClear = () => {
+    setData(data.filter((item, index) => selectedCards.includes(index)));
+  };
+
+  const handleLoad = async () => {
+    try {
+      const res = await axios.get(
+        `https://dog.ceo/api/breeds/image/random/${data.length}`
+      );
+      const newImages = res.data.message;
+      const newData = [...data];
+      newData.forEach((item, index) => {
+        if (!selectedCards.includes(index)) {
+          newData[index] = newImages.shift();
+        }
+      });
+      setData(newData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className='home'>
-        <Header onNumberChange={handleNumberChange}></Header>
-        <hr />
-        <div className='main'>
-            <Card number={number}></Card>
+      <div className='header'>
+        <input
+          className='numberInput'
+          type='text'
+          onChange={handleSetNumber}
+          value={number}
+        />
+        <button className='load' onClick={handleLoad}>
+          LOAD
+        </button>
+        <button className='clear' onClick={handleClear}>
+          CLEAR
+        </button>
+      </div>
+      <hr />
+      <div className='main'>
+        <div className='card'>
+          {data.length > 0 &&
+            data.map((item, index) => (
+              <div
+                className={`item ${selectedCards.includes(index) ? 'selected' : ''}`}
+                key={index}
+                onClick={() => handleCardSelection(index)}
+              >
+                <img src={item} alt='' />
+              </div>
+            ))}
         </div>
+      </div>
     </div>
   )
 }
